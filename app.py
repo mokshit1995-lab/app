ca8c5507c39f29de084136a291890aa320b127a9
Mokshit <mokshit.1995@gmail.com>
2026-02-01 22:00:19 +0530

    commit

---
+++ app.py
@@ -0,0 +1,20 @@
+def my_func(number):
+  for num in range(1,number,1):
+    if num % 2 == 0:
+        print(num)
+        num =+num
+    
+
+
+
+my_func(5)
+
+a=0o23335
+print((a))
+
+b=0b11010
+a=str(b)
+print("Binary number for",a, "is",b)
+
+#import keyword
+#print(keyword.kwlist)
