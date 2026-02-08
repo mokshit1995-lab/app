11a954dd7d430aaf360ddcf2211845ce7350e80e
Mokshit <mokshit.1995@gmail.com>
2026-02-08 12:13:41 +0530

    Commit

---
+++ practice.py
@@ -0,0 +1,35 @@
+import math as m
+from math import *
+from sys import *
+#print(sqrt(16))
+#print(pi)
+#r=5
+
+
+#r=int((input("Enter radius of circle:")))
+#print("Area of Circle is :", pi*(pow(r,2)))
+
+
+#a,b=[int (x) for x in input("Enter 2 Numbers:").split(sep=',')]
+#print("Sum of 2 numbers is :", a+b)
+
+#x=eval("10+13+12")
+#print(x)
+
+#ex=input("enter a expression:")
+#result=eval(ex)
+#print("The result:",result)
+
+
+#a=eval(input("Enter the data:"))
+#print(type(a))
+#print(a)
+
+#a,b,c=[eval(x) for x in input("enter 3 args:").split()]
+#print(type(a))
+#print(type(b))
+#print(type(c))
+#print(type(argv))
+
+print("length of command line arguements:",len(argv))
+print(argv[0])
