2576b0ddd044638232f0fc6d6908d5988801227e
Mokshit <mokshit.1995@gmail.com>
2026-03-29 16:48:16 +0530

    commit

--- practice1.py
+++ practice1.py
@@ -19,3 +19,54 @@ d2= { "brand": "Ford",
 print(d2)
 
 print(d2["colour"])
+
+d=dict(name='Mokshit',age=12,country="India")
+print(d)
+
+x=d["name"]
+print(type(x))
+
+x=d.get("age")
+print(x)
+
+y=d.keys()
+print(y)
+
+d['Address'] ='USA'
+
+print(y)
+
+v=d.values()
+k=d.keys()
+print(f'keys{k} and Values {v}')
+
+s=d.items()
+print(s)
+
+if 'country' in d:
+    print('Country is in the dictionary')
+
+d['Address']='Koti'
+print(d)
+
+d.update({'Address':"LOL"})
+print(d)
+
+d['colour']='Fair'
+print(d)
+
+d.update({'car':'Yes'})
+
+print(d)
+
+d.pop("Address")
+print(d)
+
+d.popitem()
+print(d)
+
+del d['colour']
+print(d)
+
+del d
+
