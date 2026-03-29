f5dd8bd4b1eced3e3b6d4f45e461e01e4e0e9db2
Mokshit <mokshit.1995@gmail.com>
2026-03-29 16:00:04 +0530

    commit

---
+++ practice1.py
@@ -0,0 +1,21 @@
+#Dictionary
+d1= { "brand": "Ford", 
+     "Model":"Mustang",
+     "year": 1987,
+     "year": 1995}
+
+print(d1)
+
+print(d1["year"])
+
+print(len(d1))
+
+d2= { "brand": "Ford", 
+     "Model":"Mustang",
+     "year": 1987,
+     "colour":["yellow","black",'Blue'],
+     "year": 1995}
+
+print(d2)
+
+print(d2["colour"])
